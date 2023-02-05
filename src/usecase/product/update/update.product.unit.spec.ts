12fe365c15089cbdcfbd924ff4950e4881f55774
Hugo Bucker <hbbucker@gmail.com>
2023-02-05 10:51:45 -0400

    ajustado teste de update do produto

--- src/usecase/product/update/update.product.unit.spec.ts
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -5,8 +5,14 @@ const product = ProductFactory.create("a", "Product Test", 9.99);
 
 const input = {
     id: product.id,
-    name: product.name,
+    name: "Product 2 Test",
     price: product.price,
+};
+
+const output_mock= {
+  id: product.id,
+  name: "Product 2 Test",
+  price: product.price,
 };
 
 const MockRepository = () => {
@@ -14,7 +20,7 @@ const MockRepository = () => {
       create: jest.fn(),
       findAll: jest.fn(),
       find: jest.fn().mockReturnValue(Promise.resolve(product)),
-      update: jest.fn(),
+      update: jest.fn().mockReturnValue(Promise.resolve(output_mock)),
     };
   };
 
